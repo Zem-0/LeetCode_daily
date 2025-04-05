27f3d183ef9f182c4071dd1f961c33d927e55ef7
Zem-0 <parinith99@gmail.com>
2025-04-05 21:22:50 +0530

    push

---
+++ maximum_xor.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        res=[]
+        res.append(0)
+        if len(nums)==0:
+            return 0
+        def backtrack(i,curr):
+            if i>=len(nums):
+                res.append(curr)
+                return res
+            take=backtrack(i+1,curr^nums[i])
+            notake=backtrack(i+1,curr)
+        backtrack(0,0)
+        return sum(res)
+
+            
+
+        
